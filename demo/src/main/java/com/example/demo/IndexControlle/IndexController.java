38da530b0175f29a2133df0bf0babca537b7aeed
zhangyifan <zhangyifan@qq.com>
2019-01-07 19:52:36 +0800

    这是提交

---
+++ demo/src/main/java/com/example/demo/IndexControlle/IndexController.java
@@ -0,0 +1,14 @@
+package com.example.demo.IndexControlle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+    @RequestMapping("index")
+    @ResponseBody
+    public String toIndex(){
+        return "index";
+    }
+}
